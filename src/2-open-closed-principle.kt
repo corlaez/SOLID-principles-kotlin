ce89c9fb9dfcdc0a4eebd8a10efaae076a0cb388
Jamie Cruwys <me@jamiecruwys.co.uk>
2020-09-08 22:25:00 +0100

    Added example 2 - open closed principle

---
+++ src/2-open-closed-principle.kt
@@ -0,0 +1,20 @@
+// Example of Open Closed Principle using Animals
+fun main() {
+    val dog = Dog("Spot")
+    dog.speak()
+    dog.woof()
+}
+
+open class Animal(private val name: String) {
+    fun speak() {
+        println("My name is $name");
+    }
+}
+
+// Dog class is able to add the woof() functionality
+// but the Dog class is unable to change the existing speak() functionality
+class Dog(dogsName: String): Animal(dogsName) {
+    fun woof() {
+        println("woof!")
+    }
+}
